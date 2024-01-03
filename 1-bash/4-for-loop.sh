b68119b90104eee564d94951b2d9be9f09b1b1d8
mayur4279 <mayurdhotre.a15@gmail.com>
2024-01-04 02:21:45 +0530

    changes in for loop file

--- 1-bash/4-for-loop.sh
+++ 1-bash/4-for-loop.sh
@@ -1,5 +1,20 @@
 
+
+#for loop for displaying the 1 to 50 numbers 
+
 for a in {1..50};
 do 
    echo $a
 done
+
+
+
+#for loop for displaying the numbers with 2 gaps  
+
+for i in {1..100..2};
+do  
+    echo $i
+done 
+
+
+
