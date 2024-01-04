d46ef45b072829dd43467ff8605a371dd34b1fde
mayur4279 <mayurdhotre.a15@gmail.com>
2024-01-05 00:12:47 +0530

    changes in 5th file

--- 1-bash/5-getting-started-with-conditionals.sh
+++ 1-bash/5-getting-started-with-conditionals.sh
@@ -2,7 +2,7 @@ read  char
 
 if  [[ $char == "y" || $char == "Y" ]];then 
    echo "YES"
-elif [[ $char == "N" || $char == "n" ]];then 
+elif [[ $char == "N" || $char == "n" ]];then     # here we using strings thats why  we uses  == operator here, for comapring numbers we are use -eq operator.... 
    echo  "NO"
 fi  
 
